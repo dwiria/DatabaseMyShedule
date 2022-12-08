7bfb2986e434526109507e8fe75109c36335f7fa
dwiria <117635194+dwiria@users.noreply.github.com>
2022-12-08 19:21:21 +0700

    Add files via upload

---
+++ myschedule.sql
@@ -0,0 +1,12 @@
+-- Database: todolist
+
+-- DROP DATABASE IF EXISTS todolist;
+
+CREATE DATABASE todolist
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United Kingdom.1252'
+    LC_CTYPE = 'English_United Kingdom.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
